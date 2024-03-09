746f2e8055ea03fca7b09ee2c93e81dc3b140661
Rafael Munareto <rafael.munareto@icloud.com>
2024-03-09 19:25:15 -0300

    tratamento de dados ok

--- classes/TratamentoDados.py
+++ classes/TratamentoDados.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import pickle
 from sklearn.preprocessing import StandardScaler
-
 class TratamentoDados:
     def __init__(self):
         self.dados = None
@@ -12,14 +11,17 @@ class TratamentoDados:
         self.dados_municipios = None
 
     def carregar_dados(self, caminhos):
-        dados_anuais = [pd.read_csv(caminho, sep=';', encoding='ISO-8859-1') for caminho in caminhos]
+        print('Carregando dados...')
+        dados_anuais = [pd.read_csv(caminho, sep=';', encoding='utf-8') for caminho in caminhos]
         self.dados = pd.concat(dados_anuais)
 
     def carregar_dados_municipios(self, caminho):
-        self.dados_municipios = pd.read_csv(caminho, sep=';', encoding='ISO-8859-1')
+        print('Carregando dados municipios...')
+        self.dados_municipios = pd.read_csv(caminho, sep=';', encoding='utf-8')     
         self.dados_municipios = self.dados_municipios[['UF', 'Nome_UF', 'Nome_Mesorregião', 'Nome_Microrregião', 'Código Município Completo', 'Nome_Município']]
     
     def tratar_dados(self):
+        print('Fazendo tratamento dos dados...')
         self.dados['DT_NOTIFIC'] = pd.to_datetime(self.dados['DT_NOTIFIC'], format='%d/%m/%Y')
         self.dados['DT_NASC'] = pd.to_datetime(self.dados['DT_NASC'], format='%d/%m/%Y', errors='coerce')
         self.dados['ANO'] = self.dados['DT_NOTIFIC'].dt.year
@@ -28,6 +30,7 @@ class TratamentoDados:
         self.dados = self.dados.dropna(subset=['CO_REGIONA', 'DT_NASC'])
 
     def mesclar_dados_municipios(self):
+        print('Mesclando dados de municipio com os dados gerais...')
         # Assegurar que os códigos de municípios estejam no formato correto
         self.dados['CO_MUN_NOT'] = self.dados['CO_MUN_NOT'].astype(str)
         self.dados_municipios['Código Município Completo'] = self.dados_municipios['Código Município Completo'].astype(str)
@@ -36,11 +39,13 @@ class TratamentoDados:
         self.dados = pd.merge(self.dados, self.dados_municipios, how='left', left_on='CO_MUN_NOT', right_on='Código Município Completo')
 
     def preparar_previsores_alvo(self):
+        print('Criando previsores e alvo...')
         dados_agregados = self.dados.groupby(['ANO', 'MES', 'Nome_UF', 'Nome_Mesorregião', 'Nome_Microrregião', 'Nome_Município']).size().reset_index(name='CASOS')
         self.previsores = dados_agregados.drop('CASOS', axis=1)
         self.alvo = dados_agregados['CASOS']
 
     def escalonar_salvar_previsores_alvo(self):
+        print('Escalonando...')
         scaler = StandardScaler()
         previsores_escalados = scaler.fit_transform(self.previsores)
         with open('./variaveis/previsores.pickle', 'wb') as f:
